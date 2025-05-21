bc96df4afc408bf169c1e2c7dfdfad12adf47046
Hans Windisch <hans-michael.windisch@thi.de>
2025-05-21 16:46:18 +0200

    lecture 21.5.2025

--- chapter9/HelloWorld.java
+++ chapter9/HelloWorld.java
@@ -1,12 +1,27 @@
 package programming2.chapter9;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+
+class ButtonHandler implements EventHandler<ActionEvent> {
+	private TextField nameTextField;
+	
+	public ButtonHandler(TextField nameTextField) {
+		this.nameTextField = nameTextField;
+	}
+	
+	public void handle(ActionEvent event) {
+		String name = nameTextField.getText();
+		System.out.println("Var 2: Hello " + name + "!");
+	}	
+}
 
 public class HelloWorld extends Application {
 
@@ -22,19 +37,43 @@ public class HelloWorld extends Application {
 		// create controls
 		TextField nameTextField = new TextField();
 		Button sayHelloButton = new Button("greet");
+
+		Button exitButton = new Button("exit");
 		
 		// layout it
-		pane.getChildren().addAll(new Label("Name: "), nameTextField, sayHelloButton);
+		pane.getChildren().addAll(new Label("Name: "), nameTextField, sayHelloButton, exitButton);
+		
+		// styling: make flowpane bavckground yellow
+		pane.setStyle("-fx-background-color: yellow");
 		
 		// handle button click event
 		sayHelloButton.setOnAction(event -> {
 			String name = nameTextField.getText();
-			System.out.println("Hello " + name + "!");
+			System.out.println("Var 1: Hello " + name + "!");
 		});
+		
+		// 2. variant using handler class => lots of boiler plate code!
+		sayHelloButton.setOnAction(new ButtonHandler(nameTextField));
+		
+		// 3. variant using anonymous class
+		EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent arg0) {
+				String name = nameTextField.getText();
+				System.out.println("Var 3: Hello " + name + "!");
+			}
+		};
+		sayHelloButton.setOnAction(handler);
+		
+		// define scene 2
+		Button hiButton = new Button("Hi!");
+		Scene scene2 = new Scene(hiButton);
+		//exitButton.setOnAction(e -> window.setScene(scene2));
+		exitButton.setOnAction(e -> window.hide());
 		
 		// show dialog in a scene
 		Scene scene = new Scene(pane, 300, 50);
 		window.setScene(scene);
+		window.setResizable(true);
 		window.show();
 	}
 	
